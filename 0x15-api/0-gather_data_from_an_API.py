f5fb0c24537786b1bc0a0f4258403c214dfb368f
BrianKvin <briankvin@gmail.com>
2023-04-24 13:21:19 -0700

    JSON as placeholder

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,6 @@
 #!/usr/bin/python3
 """ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
-
-import requests
-import sys
-
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    # response = requests.get
-    (f"https://jsonplaceholder.typicode.com/users/{user_id}")
-
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    response = get(url)
-    name = response.json().get('name')
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'
-    .format(user_id)
-    response = get(url)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
-
-    print("Employee {} is done with task({}/{}):"
-        .format(name, done, len(tasks)))
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
-"""
-
-#!/usr/bin/python3
-"""
-Uses the JSON placeholder api to query data about an employee
 """
 
 from requests import get
