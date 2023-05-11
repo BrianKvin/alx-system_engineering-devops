4ab8c25afdea742edbedef9aeb3942f5427ff9a0
Bkelvin <briankvin@gmail.com>
2023-05-11 21:32:11 +0300

    Edited version

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+"""def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
@@ -29,4 +29,32 @@ def recurse(subreddit, hot_list=[], after="", count=0):
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    return hot_list"""
+
+def recurse(subreddit, hot_list=[]):
+    """queries the Reddit API
+
+    args:
+        subreddit - subreddit name
+
+    returns a list containing the titles of all hot articles for a given
+    subreddit"""
+    global after
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    header = {'User-Agent': 'Title/0.1 by JI-Maina'}
+    param = {'after': after}
+
+    res = requests.get(
+            url, headers=header, params=param, allow_redirects=False)
+
+    if res.status_code == 200:
+        next = res.json().get('data').get('after')
+        if next is None:
+            after = next
+            recurse(subreddit, hot_list)
+        list_titles = res.json().get('data').get('children')
+        for title in list_titles:
+            hot_list.append(title.get('data').get('title'))
+        return hot_list
+    else:
+        return (None)
